0be44cea2ee3759399cf00b0415de75cf2fcc0e9
Tim Marston <tim.marston@lockwood-publishing.com>
2020-06-15 12:52:52 +0100

    switched out fn to convert tarantool-style cdata uint64s

--- pb.c
+++ pb.c
@@ -276,34 +276,71 @@ static int lpb_hexchar(char ch) {
     return -1;
 }
 
-#ifdef HAVE_LUAJIT_SRC /* LuaJIT sources */
+#ifdef HAVE_TARANTOOL /* tarantool sources */
 
 #include <lj_ctype.h>
 #include <lj_obj.h>
 
-static uint64_t lpb_ljcdata_toint(lua_State *L, int idx, int *isint) {
-	if (idx < 0)
-		idx = lua_gettop(L) + idx + 1;
-	GCcdata *cd = cdataV(L->base + idx - 1);
-	uint64_t v = 0;
-	switch (cd->ctypeid) {
-	case CTID_CCHAR:
-	case CTID_INT8: v = *(int8_t *)((void *)cdataptr(cd)); break;
-	case CTID_INT16: v = *(int16_t *)((void *)cdataptr(cd)); break;
-	case CTID_INT32: v = *(int32_t *)((void *)cdataptr(cd)); break;
-	case CTID_INT64: v = *(int64_t *)((void *)cdataptr(cd)); break;
-	case CTID_UINT8: v = *(uint8_t *)((void *)cdataptr(cd)); break;
-	case CTID_UINT16: v = *(uint16_t *)((void *)cdataptr(cd)); break;
-	case CTID_UINT32: v = *(uint32_t *)((void *)cdataptr(cd)); break;
-	case CTID_UINT64: v = *(uint64_t *)((void *)cdataptr(cd)); break;
-	default:
-		*isint = 0;
+static inline int
+luaL_convertint64(lua_State *L, int idx, bool unsignd, int64_t *result)
+{
+	uint32_t ctypeid;
+	void *cdata;
+	/*
+	 * This code looks mostly like luaL_tofield(), but has less
+	 * cases and optimized for numbers.
+	 */
+	switch (lua_type(L, idx)) {
+	case LUA_TNUMBER:
+		*result = lua_tonumber(L, idx);
 		return 0;
+	case LUA_TCDATA:
+		cdata = luaL_checkcdata(L, idx, &ctypeid);
+		switch (ctypeid) {
+		case CTID_CCHAR:
+		case CTID_INT8:
+			*result = *(int8_t *) cdata;
+			return 0;
+		case CTID_INT16:
+			*result = *(int16_t *) cdata;
+			return 0;
+		case CTID_INT32:
+			*result = *(int32_t *) cdata;
+			return 0;
+		case CTID_INT64:
+			*result = *(int64_t *) cdata;
+			return 0;
+		case CTID_UINT8:
+			*result = *(uint8_t *) cdata;
+			return 0;
+		case CTID_UINT16:
+			*result = *(uint16_t *) cdata;
+			return 0;
+		case CTID_UINT32:
+			*result = *(uint32_t *) cdata;
+			return 0;
+		case CTID_UINT64:
+			*result = *(uint64_t *) cdata;
+			return 0;
+		}
+		*result = 0;
+		return -1;
+	case LUA_TSTRING:
+	{
+		const char *arg = luaL_checkstring(L, idx);
+		char *arge;
+		errno = 0;
+		*result = (unsignd ? (long long) strtoull(arg, &arge, 10) :
+			strtoll(arg, &arge, 10));
+		if (errno == 0 && arge != arg)
+			return 0;
+		return 1;
 	}
-	*isint = 1;
-	return v;
+	}
+	*result = 0;
+	return -1;
 }
-#endif /* LuaJIT sources */
+#endif /* tarantool sources */
 
 static uint64_t lpb_tointegerx(lua_State *L, int idx, int *isint) {
     int neg = 0;
@@ -313,12 +350,12 @@ static uint64_t lpb_tointegerx(lua_State *L, int idx, int *isint) {
     if (*isint) return v;
 #else
     uint64_t v = 0;
-#ifdef HAVE_LUAJIT_SRC /* LuaJIT sources */
-    if (lua_type(L, idx) == LUA_TCDATA) {
-	    v = lpb_ljcdata_toint(L, idx, isint);
-	    if (*isint) return v;
+#ifdef HAVE_TARANTOOL /* tarantool sources */
+    if( luaL_convertint64( L, idx, true, &v ) == 0 ) {
+	    *isint = true;
+	    return v;
     }
-#endif /* LuaJIT sources */
+#endif /* tarantool sources */
     lua_Number nv = lua_tonumberx(L, idx, isint);
     if (*isint) {
         if (nv < (lua_Number)INT64_MIN || nv > (lua_Number)INT64_MAX)
